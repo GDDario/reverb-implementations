187bf563aa1092d34ff76bb66199af4118473f3f
GDDario <gd.dario.dev@gmail.com>
2025-08-25 20:40:16 -0300

    feat(react): delayed event communication example

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,17 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import DelayedProgressBlock from "./components/blocks/DelayedProgressBlock.tsx";
+import {configureEcho} from "@laravel/echo-react";
+
+// window.Pusher = Pusher;
+//
+// window.Echo = new Echo({
+//     broadcaster: 'reverb',
+//     key: import.meta.env.VITE_REVERB_APP_KEY,
+//     wsHost: import.meta.env.VITE_REVERB_HOST,
+//     wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
+//     wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
+//     forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+//     enabledTransports: ['ws', 'wss'],
+// });
+
+configureEcho({
+    broadcaster: 'reverb',
+    key: 'u7lwdgqskhaowu5zek73',
+    wsHost: '0.0.0.0',
+    wsPort: 8080,
+    wssPort: 443,
+    forceTLS: false,
+    enabledTransports: ['ws'],
+    // authEndpoint: null,
+    // auth: null,
+});
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-        <DelayedProgressBlock />
-    </>
-  )
+    return (
+        <>
+            <DelayedProgressBlock/>
+        </>
+    )
 }
 
 export default App
--- react-app/src/components/blocks/DelayedProgressBlock.tsx
+++ react-app/src/components/blocks/DelayedProgressBlock.tsx
@@ -1,11 +1,88 @@
-import * as React from "react";
+import {useEffect, useState} from "react";
+import echo from "laravel-echo";
+import {echoIsConfigured, useEcho, useEchoPublic} from "@laravel/echo-react";
+
+type DelayedEventResponse = {
+    key: string;
+}
+
+type DelayedEvent = {
+    key: string;
+    dateTime: string;
+};
 
 export default function DelayedProgressBlock() {
-    return (
-        <section className="card">
-            <h2>Delayed Progress Block</h2>
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+    const [isInProgress, setIsInProgress] = useState<boolean>(false);
+    const [events, setEvents] = useState<DelayedEvent[]>([]);
 
-            aaaasdasd
+    const {listen} = useEchoPublic(
+        'delayed-event-channel',
+        '.delayed-event',
+        (e: DelayedEventResponse) => {
+            // setEvents((prev) => [...prev, {
+            //     key: e.key,
+            //     dateTime: Date.now(),
+            // }])
+            setEvents((prev: DelayedEvent[]) => [
+                ...prev,
+                {
+                    key: e.key,
+                    dateTime: new Date().toLocaleTimeString(),
+                }
+            ]);
+            setIsInProgress(false);
+            console.log('Event received: ', e);
+        }
+    );
+
+    useEffect(() => {
+        listen();
+    }, []);
+
+    const startLongTask = () => {
+        setIsLoading(true);
+        setIsInProgress(true);
+        fetch('http://localhost:8000/api/delayed-event').then(() => setIsLoading(false));
+    }
+
+    return (
+        <section
+            className="card flex flex-col gap-4"
+            title="This block demonstrates handling delayed events with real-time update when completed."
+        >
+            <div className="flex gap-2 justify-between">
+                <h2>Delayed Progress Block</h2>
+
+                {
+                    isInProgress && (
+                        <div className="flex items-center gap-2">
+                            <div className="w-4 h-4 border-4 border-blue-600 border-t-transparent rounded-full animate-spin"></div>
+                            <span className="text-sm text-gray-600">In Progress...</span>
+                        </div>
+                    )
+                }
+            </div>
+
+            <div className="flex flex-col gap-1">
+                {
+                    events.map((event: DelayedEvent) => (
+                        <div key={event.key} className="p-2 bg-slate-800 rounded">
+                            <span className="font-mono text-sm">Event Key: {event.key}</span>
+                            <br />
+                            <span className="text-xs text-gray-600">Received at: {event.dateTime}</span>
+                        </div>
+                    ))
+                }
+            </div>
+
+            <button
+                className="px-4 py-2 rounded bg-blue-800 text-white font-semibold shadow hover:bg-blue-900 transition-colors cursor-pointer"
+                onClick={startLongTask}
+                disabled={isLoading}
+            >
+                {isLoading ? 'Starting ...' : 'Start Long Task'}
+            </button>
         </section>
     );
 }
