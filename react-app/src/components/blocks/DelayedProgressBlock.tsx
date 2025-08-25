3e808f090cb08fcd75d538f3a5f6888c10f7a397
GDDario <gd.dario.dev@gmail.com>
2025-08-25 13:54:25 -0300

    chore(react): initial react design idea

---
+++ react-app/src/components/blocks/DelayedProgressBlock.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+export default function DelayedProgressBlock() {
+    return (
+        <section className="card">
+            <h2>Delayed Progress Block</h2>
+
+            aaaasdasd
+        </section>
+    );
+}
